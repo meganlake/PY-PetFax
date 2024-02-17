65b15a0a5113a4facd5ea7bcb285edf797c9b1a6
Megan Lake <meganlake95@gmail.com>
2024-02-17 14:44:18 -0800

    linked database

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask 
+from flask_migrate import Migrate
 
 def create_app():
     # config
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Kauai28**@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from . import models
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
 
     # index route
     @app.route('/')
